ad64b81a9d9314c745814203412a65361ae0ba10
Sofyan Sitorus <sofyansitorus@gmail.com>
2022-03-17 13:09:50 +0700

    Strip special characters

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ export default function App() {
       replacement: separator, // replace spaces with replacement character, defaults to `-`
       remove: undefined, // remove characters that match regex, defaults to `undefined`
       lower: true, // convert to lower case, defaults to `false`
-      strict: false, // strip special characters except replacement, defaults to `false`
+      strict: true, // strip special characters except replacement, defaults to `false`
       locale: "en", // language code of the locale to use
       trim: true // trim leading and trailing replacement chars, defaults to `true`
     });
