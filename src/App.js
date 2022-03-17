7cf089f6c163967247043467b78e72d7fbe77b72
Sofyan Sitorus <sofyansitorus@gmail.com>
2022-03-17 14:39:35 +0700

    Improve copy to clipboard

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import { useState, useEffect } from "react";
-import slugify from "slugify";
-import CopyToClipboard from "react-copy-to-clipboard";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState, useRef } from "react";
 import {
-  ThemeProvider,
-  Navbar,
+  Button,
+  Col,
   Container,
   Form,
   FormControl,
   InputGroup,
-  Button,
+  Navbar,
   Row,
-  Col
+  Spinner,
+  ThemeProvider,
+  Card
 } from "react-bootstrap";
-
-import "bootstrap/dist/css/bootstrap.min.css";
+import { CheckCircleFill, Github } from "react-bootstrap-icons";
+import CopyToClipboard from "react-copy-to-clipboard";
+import slugify from "slugify";
 import "./styles.css";
 
 const limitMin = 10;
@@ -26,6 +28,11 @@ export default function App() {
   const [limit, setLimit] = useState(limitDefault);
   const [separator, setSeparator] = useState(defaultSeparator);
   const [result, setResult] = useState("");
+  const [copied, setCopied] = useState(false);
+  const [spinning, setSpinning] = useState(false);
+
+  const disableSpinnerTimer = useRef();
+  const toggleCopiedTimer = useRef();
 
   const onChangeText = (event) => {
     setText(event.target.value);
@@ -55,6 +62,19 @@ export default function App() {
     }
   };
 
+  const onCopy = () => {
+    if (disableSpinnerTimer.current) {
+      clearTimeout(disableSpinnerTimer.current);
+    }
+
+    if (toggleCopiedTimer.current) {
+      clearTimeout(toggleCopiedTimer.current);
+    }
+
+    setCopied(true);
+    setSpinning(true);
+  };
+
   useEffect(() => {
     const slugified = slugify(text, {
       replacement: separator, // replace spaces with replacement character, defaults to `-`
@@ -68,68 +88,129 @@ export default function App() {
     setResult(slugified.substring(0, limit));
   }, [text, limit, separator]);
 
+  useEffect(() => {
+    if (copied) {
+      const disableSpinner = () => {
+        return new Promise((resolve) => {
+          disableSpinnerTimer.current = setTimeout(() => {
+            setSpinning(false);
+            resolve();
+          }, 300);
+        });
+      };
+
+      disableSpinner().then(() => {
+        toggleCopiedTimer.current = setTimeout(() => {
+          setCopied(false);
+        }, 1000);
+      });
+    }
+  }, [copied]);
+
+  useEffect(() => {
+    return () => {
+      if (disableSpinnerTimer.current) {
+        clearTimeout(disableSpinnerTimer.current);
+      }
+
+      if (toggleCopiedTimer.current) {
+        clearTimeout(toggleCopiedTimer.current);
+      }
+    };
+  }, []);
+
+  const getCopyText = () => {
+    if (copied) {
+      if (spinning) {
+        return <Spinner animation="border" size="sm" />;
+      }
+
+      return <CheckCircleFill size={24} />;
+    }
+
+    return "Copy";
+  };
+
+  const textIsEmpty = 1 > text.length;
+  const copyDisabled = copied || textIsEmpty;
+  const copyVariant = textIsEmpty ? "secondary" : "primary";
+
   return (
     <ThemeProvider breakpoints={["sm", "xs", "xxs"]}>
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="light" expand="lg" className="mb-5">
         <Container>
           <Navbar.Brand href="#home">Slugifier</Navbar.Brand>
+          <Navbar.Text className="justify-content-end">
+            <a
+              href="https://github.com/sofyansitorus/slugifier/fork"
+              title="Fork on GitHub"
+            >
+              <Github />
+            </a>
+          </Navbar.Text>
         </Container>
       </Navbar>
       <Container>
-        <Form>
-          <Row>
-            <Col xs={7}>
-              <Form.Group className="mb-3" controlId="textInput">
-                <Form.Label>Text</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter text to slugify"
-                  size="lg"
-                  value={text}
-                  onChange={onChangeText}
-                />
-              </Form.Group>
-            </Col>
-            <Col xs={2}>
-              <Form.Group className="mb-3" controlId="inputLimit">
-                <Form.Label>Limit</Form.Label>
-                <Form.Control
-                  type="number"
-                  min="10"
-                  placeholder="Limit"
-                  size="lg"
-                  value={limit}
-                  onChange={onChangeLimit}
-                  onBlur={onBlurLimit}
-                />
-              </Form.Group>
-            </Col>
-            <Col xs={3}>
-              <Form.Group className="mb-3" controlId="inputSeparator">
-                <Form.Label>Separator</Form.Label>
-                <Form.Select
-                  aria-label="Slug separator"
-                  size="lg"
-                  value={separator}
-                  onChange={onChangeSeparator}
-                >
-                  <option value="-">Dash</option>
-                  <option value="_">Underscore</option>
-                </Form.Select>
-              </Form.Group>
-            </Col>
-          </Row>
+        <Card className="p-5">
+          <Form>
+            <Row>
+              <Col xs={7}>
+                <Form.Group className="mb-3" controlId="textInput">
+                  <Form.Label>Text</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter text to slugify"
+                    size="lg"
+                    value={text}
+                    onChange={onChangeText}
+                  />
+                </Form.Group>
+              </Col>
+              <Col xs={2}>
+                <Form.Group className="mb-3" controlId="inputLimit">
+                  <Form.Label>Limit</Form.Label>
+                  <Form.Control
+                    type="number"
+                    min="10"
+                    placeholder="Limit"
+                    size="lg"
+                    value={limit}
+                    onChange={onChangeLimit}
+                    onBlur={onBlurLimit}
+                  />
+                </Form.Group>
+              </Col>
+              <Col xs={3}>
+                <Form.Group className="mb-3" controlId="inputSeparator">
+                  <Form.Label>Separator</Form.Label>
+                  <Form.Select
+                    aria-label="Slug separator"
+                    size="lg"
+                    value={separator}
+                    onChange={onChangeSeparator}
+                  >
+                    <option value="-">Dash</option>
+                    <option value="_">Underscore</option>
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+            </Row>
 
-          <Form.Group className="mb-3" controlId="textResult">
-            <Form.Label>Result</Form.Label>
-            <InputGroup className="mb-3">
-              <FormControl type="text" size="lg" value={result} readOnly />
-              <CopyToClipboard text={result}>
-                <Button>Copy</Button>
-              </CopyToClipboard>
-            </InputGroup>
-          </Form.Group>
-        </Form>
+            <Form.Group className="mb-3" controlId="textResult">
+              <Form.Label>Result</Form.Label>
+              <InputGroup className="mb-3">
+                <FormControl type="text" size="lg" value={result} readOnly />
+                <CopyToClipboard
+                  text={result}
+                  onCopy={onCopy}
+                  disabled={copyDisabled}
+                >
+                  <Button variant={copyVariant}>{getCopyText()}</Button>
+                </CopyToClipboard>
+              </InputGroup>
+            </Form.Group>
+          </Form>
+        </Card>
       </Container>
     </ThemeProvider>
   );
